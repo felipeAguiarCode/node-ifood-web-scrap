d974111598882c52541c490d4da0ff985048d517
felipeAguiarCode <felipe.silva.aguiar047@gmail.com>
2023-05-26 16:55:37 -0300

    headless

--- src/lib/getToken.js
+++ src/lib/getToken.js
@@ -8,16 +8,6 @@ export async function getTokenCollected() {
   const HTTP_INTERCEPTED = '/catalog?category_items_size=12'
 
   const browser = await puppeteer.launch({
-    args: [
-      "--disable-setuid-sandbox",
-      "--no-sandbox",
-      "--single-process",
-      "--no-zygote",
-    ],
-    executablePath:
-      process.env.NODE_ENV === 'production'
-        ? process.env.PUPPETEER.EXECUTABLE_PATH
-        : puppeteer.executablePath(),
     headless: "new"
   })
 
